1a0591d7cc1601453ef0849ce32da9c4a5ede2c7
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-05-06 10:33:27 -0500

    Merge branch 'master' of https://github.com/Lambda-School-Labs/voice-chatroom-be

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const cors = require("cors")
+const express = require("express")
+const { ApolloServer, gql } = require ('apollo-server-express');
+
+const app = express();
+
+// app.use(cors());
+
+const schema = gql`
+    type Query{
+        me: User
+    }
+
+    type User {
+        username: String
+    }
+    `;
+
+// Resolvers are used to return data for fields from a schema
+const resolvers = {
+    Query: {
+        me: () =>{
+            return{
+                username: 'Test User',
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+// Using applymiddleware feature allows us to opt in any middleware.
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () =>{
+    console.log('Apollo Server on http://localhost:8000/graphql')
+})
