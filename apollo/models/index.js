2de9f8fcd1d838e98aade396092b7b3900c8514c
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-05-07 20:11:47 -0500

    Apollo User schema and resolvers

---
+++ apollo/models/index.js
@@ -0,0 +1,31 @@
+const users = {
+    1: {
+        id: '1',
+        username: 'Test Mctestface',
+        messageIds: [1],
+    },
+    2: {
+        id: '2',
+        username: 'John Doe',
+        messageIds: [2],
+    },
+};
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2',
+    },
+};
+
+module.exports = {
+    users,
+    messages,
+};
+
