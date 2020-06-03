d0619fda035d6506700b5c2ed7917f7476e0dbdf
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-06-03 12:10:24 -0500

    mentor table

--- data/migrations/20200514231319_users.js
+++ data/migrations/20200514231319_users.js
@@ -1,19 +1,34 @@
 exports.up = async function (knex, promise) {
   await knex.schema
-    .createTable("users", (tbl) => {
+    .createTable('users', (tbl) => {
     tbl.increments().unsigned().primary();
-    tbl.string("email").unique().notNullable();
-    tbl.string("given_name").notNullable();
-    tbl.string("family_name").notNullable();
-    tbl.string("username");
-    tbl.string("location");
-    tbl.string("createdAt");
-    tbl.string("interest_1");
-    tbl.string("interest_2");
-    tbl.string("interest_3");
-    tbl.string("avatar")
-  });
+    tbl.string('email').unique().notNullable();
+    tbl.string('given_name').notNullable();
+    tbl.string('family_name').notNullable();
+    tbl.string('username');
+    tbl.string('location');
+    tbl.timestamp('created_at', { useTz: true });
+    tbl.string('interest_1');
+    tbl.string('interest_2');
+    tbl.string('interest_3');
+    tbl.binary('avatar');
+    tbl.boolean('isMentor').defaultTo(false);
+  })
+  .createTable('mentors', tbl =>{
+    tbl.increments().unsigned().primary();
+    tbl.string('category_1');
+    tbl.string('category_2');
+    tbl.string('category_3');
+    tbl.string('mentor_rating');
+    tbl.string('mentee_list')
+    .unsigned()
+    .references('email')
+    .inTable('users');
+    
+  })
 };
 exports.down = async function (knex, promise) {
-  await knex.schema.dropTableIfExists("users");
+  await knex.schema
+  .dropTableIfExists('users')
+  .dropTableIfExists('mentors');
 };
