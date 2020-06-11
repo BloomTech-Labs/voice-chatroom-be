0b4663487c7a85dfb1dd2467285cb58189685ce3
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-06-11 14:00:19 -0500

    added supertest, set up initial test

--- _tests_/users.test.js
+++ _tests_/users.test.js
@@ -1,25 +1,60 @@
-const request = require("supertest");
-const server = require("../app");
-const db = require('../data/config')
+// const request = require("supertest");
+// const server = require("../app");
+// const db = require('../data/config')
 
-beforeEach(async () => {
-  await  db('users').del().whereBetween('id', [1,100])// This is deleting users in table
-})
-// afterEach(() => {
+// beforeEach(async () => {
+//   await  db('users').del().whereBetween('id', [1,100])// This is deleting users in table
+// })
+// // afterEach(() => {
 
+// // // })
+
+// test("creating a user", async () => {
+//   await request(server)
+//     .post("/users")
+//     .send({
+//       email: "happy@testing.org",
+//       first_name: "MC",
+//       last_name: "Hammer",
+//     })
+//     .expect(201);
+// });
+
+// test("Should log in user", async () => {
+//     await request(server).post("/users")
 // })
 
-test("creating a user", async () => {
-  await request(server)
-    .post("/users")
-    .send({
-      email: "happy@testing.org",
-      first_name: "MC",
-      last_name: "Hammer",
-    })
-    .expect(201);
-});
+const app = require('../app')
+const supertest = require('supertest')
+const request = supertest(app)
+const knex = require('knex')(config);
 
-test("Should log in user", async () => {
-    await request(server).post("/users")
+it('Gets the welcome endpoint', async done =>{
+  const response = await request.get('/')
+
+  expect(response.status).toBe(200)
+  expect(response.body.message).toBe('Nice.')
+  done()
 })
+
+
+
+
+describe('test suite description', () => {
+  beforeAll(() => {
+    return knex.migrate.latest();
+    return knex.seed.run()
+  });
+  afterAll(() => {
+    return knex.migrate
+      .rollback()
+      .then(() => knex.destroy());
+  });
+
+  test('test 1', () => {
+    // testing stuff
+  });
+  test('test 2', () => {
+    // testing other stuff
+  });
+});
