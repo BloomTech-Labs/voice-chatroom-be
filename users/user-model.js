6a03122f1d8ed041393de15cdda7af8921feaf64
Tony <tonyrkovar@gmail.com>
2020-06-23 19:49:52 -0500

    chore: linter auto format && remove bad console.logs

--- users/user-model.js
+++ users/user-model.js
@@ -1,58 +1,42 @@
-const db = require('../data/config')
+const db = require("../data/config");
 
-function find(){
-    return db("users")
+function find() {
+	return db("users");
 }
 // selects all users from users table
 
-function findById(id){
-    return db("users")
-    .where("id", id)
-    // .limit(1)
-
+function findById(id) {
+	return db("users").where("id", id);
+	// .limit(1)
 }
 // Matches ID value within users table
 
-function findByEmail(email){
-    console.log(email)
-    return db("users")
-    .where("email", email)
+function findByEmail(email) {
+	return db("users").where("email", email);
 }
 
-function add(user){
-    return db("users")
-    .insert(user)
+function add(user) {
+	return db("users").insert(user);
 }
 
-function update(changes, id){
-    return db("users")
-    .update(changes)
-    .where("id", id)
+function update(changes, id) {
+	return db("users").update(changes).where("id", id);
 }
 
-function remove(id){
-    return db("users")
-    .del()
-    .where("id", id)
+function remove(id) {
+	return db("users").del().where("id", id);
 }
 
-function makeMentor(changes,id){
-    
-    return db("users")
-    .where("id", id)
-    .update(changes)
+function makeMentor(changes, id) {
+	return db("users").where("id", id).update(changes);
 }
 
-
-
-
-module.exports ={
-    find,
-    findById,
-    add,
-    update,
-    remove,
-    makeMentor,
-    findByEmail
-}
-
+module.exports = {
+	find,
+	findById,
+	add,
+	update,
+	remove,
+	makeMentor,
+	findByEmail,
+};
